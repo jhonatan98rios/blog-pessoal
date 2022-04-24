184acfec13899c53a5a2013eca45a6c31980a946
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2022-04-24 05:26:51 -0300

    Header mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import useDeviceDetect from '../../hooks/useDevice';
 import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
@@ -5,6 +6,17 @@ import styles from './styles.module.scss';
 export function Header() {
 
   const {isMobile} = useDeviceDetect()
+  const [checked, setChecked] = useState(false);
+
+  function handleCheckbox() {
+    if (checked) {
+      setChecked(false)
+      document.querySelector('body').style.overflowY = 'auto'
+    } else {
+      setChecked(true)
+      document.querySelector('body').style.overflowY = 'hidden'
+    }
+  }
 
   return (
     <header className={styles.container}>
@@ -13,17 +25,24 @@ export function Header() {
         {
           isMobile && (
             <div className={styles.menuIcon}>
-              <input type="checkbox" id="icon_menu" className={styles.icon_menu} />
+              <input 
+                type="checkbox" 
+                id="icon_menu" 
+                className={styles.icon_menu} 
+                checked={checked}
+                onChange={handleCheckbox}
+              />
               <label htmlFor="icon_menu">
                 <div></div>
                 <div></div>
                 <div></div>
-              </label>  
+              </label>
             </div> 
           )
         }
 
         <input className={styles.input} type="text" placeholder="Pesquisar pelo post" />
+
         {
           !isMobile && (
             <nav>
@@ -40,6 +59,25 @@ export function Header() {
                 <a>Social</a>
               </ActiveLink>
             </nav>
+          )
+        }
+
+        {
+          isMobile && checked && (
+            <nav className={styles.navbar}>
+              <ActiveLink href="/" activeClassName={styles.active}>
+                <a>Sobre</a>
+              </ActiveLink>
+              <ActiveLink href="/" activeClassName={styles.active}>
+                <a>Tecnologias</a>
+              </ActiveLink>
+              <ActiveLink href="/posts" activeClassName={styles.active}>
+                <a>Blog</a>
+              </ActiveLink>
+              <ActiveLink href="/" activeClassName={styles.active}>
+                <a>Social</a>
+              </ActiveLink>
+            </nav> 
           )
         }
       </div>
--- src/hooks/useDevice.ts
+++ src/hooks/useDevice.ts
@@ -3,9 +3,14 @@ import React from "react";
 export default function useDeviceDetect() {
   const [isMobile, setMobile] = React.useState(false);
 
-  React.useEffect(() => {
-    const mobile = window.innerWidth <= 768
+  function deviceVerification() {
+    const mobile = window.innerWidth <= 1024
     setMobile(mobile);
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("resize", deviceVerification);
+    return () => window.removeEventListener("resize", deviceVerification);
   }, []);
 
   return { isMobile }
