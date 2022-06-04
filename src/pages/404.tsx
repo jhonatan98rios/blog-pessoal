63be2e9460e12a8116b6edda50b12e07ac71d276
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2022-06-04 14:07:05 -0300

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,23 @@
 export default function NotFound() {
   return (
-    <div>
-      <h1>Page not found</h1>
-    </div>
+    <main style={main}>
+      <img style={image} src="404.gif" alt="meme john travolta" />
+      <h1>404 - Pagina não encontrada</h1>
+    </main>
   );
+}
+
+const main = {
+  'height': 'calc(100vh - 160px)',
+  'width': '100vw',
+  'display': 'flex',
+  'justify-content': 'center',
+  'align-items': 'center',
+  'flex-direction': 'column',
+  'padding-top': '32px',
+}
+
+const image = {
+  'margin-bottom': '32px',
+  'height': '320px'
 }
