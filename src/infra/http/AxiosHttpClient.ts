e9fcbff1df4deb2513004ea4aa77e38d7a688409
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-08-31 22:13:20 -0300

    trying to fix CORS error

--- src/infra/http/AxiosHttpClient.ts
+++ src/infra/http/AxiosHttpClient.ts
@@ -42,8 +42,7 @@ export class AxiosHttpClient implements AbstractHttpClient<AxiosInstance> {
       const config = {
         headers: {
           'Content-Type': 'application/json',
-          'Origin': 'https://jhonatan-dev-rios-blog.com.br/',
-          'Access-Control-Allow-Credentials': true,
+          'Origin': 'https://jhonatan-dev-rios-blog.com.br',
         }
       }
 
